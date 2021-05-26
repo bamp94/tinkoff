764b9eddcab6c31ac220141148af29d8b650154e
Roman Vlasenko <bamp94@gmail.com>
2021-05-26 12:23:37 +0300

    change amount type

--- init.go
+++ init.go
@@ -31,7 +31,7 @@ type InitRequest struct {
 
 type Shop struct {
 	ShopCode string `json:"ShopCode,omitempty"` // Код магазина. Для параметра ShopСode необходимо использовать значение параметра Submerchant_ID, полученного при регистрации через xml.
-	Amount   string `json:"Amount,omitempty"`   // Суммаперечисленияв копейкахпо реквизитам ShopCode за вычетом Fee
+	Amount   uint64 `json:"Amount,omitempty"`   // Суммаперечисленияв копейкахпо реквизитам ShopCode за вычетом Fee
 	Name     string `json:"Name,omitempty"`     // Наименованиепозиции
 	Fee      string `json:"Fee,omitempty"`      // Часть суммы Операции оплаты или % от суммы Операции оплаты. Fee удерживается из возмещения третьего лица (ShopCode) в пользу Предприятия по операциям оплаты.
 }
